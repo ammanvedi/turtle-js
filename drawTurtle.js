f20f911ae5c0de8d53f408b0e74ff29811ff0ae5
Amman Vedi <amman.vedi@gmail.com>
2013-11-12 22:13:45 +0000

    made code pretty

--- drawTurtle.js
+++ drawTurtle.js
@@ -1,111 +1,96 @@
 
-
-
-        var dxincrement = 5;
-        var dyincrement = 2;
-        var rot_increment = 5;
-
+       
+        var dxincrement =     5;
+        var dyincrement =     2;
+        var rot_increment =   5;
+        var dr =              0;
 
         var turtle = {
-            init_x: 100,
-            init_y: 100,
-            bound_width: 50,
-            bound_height: 60,
-            ctr_x: 125,
-            ctr_y: 130,
-            x: 100,
-            y: 100
+            init_x:        100,
+            init_y:        100,
+            bound_width:   50,
+            bound_height:  60,
+            ctr_x:         125,
+            ctr_y:         130,
+            x:             100,
+            y:             100
         };
 
         var keys = {
-            up: false,
-            down: false, 
-            left: false, 
-            right: false
+            up:     false,
+            down:   false, 
+            left:   false, 
+            right:  false
         };
 
-        
         //delegate events to JS functions
         document.onkeydown = checkKey;
         document.onkeyup = removeKey;
  
-
         //function to keep track of the centre of the turtle
         function updateTurtleCentre()
         {
             turtle['ctr_x'] = turtle['init_x'] + dx + (turtle['bound_width'] / 2);
             turtle['ctr_y'] = turtle['init_y'] + dy + (turtle['bound_height'] / 2);;
-        }
+        };
 
         //draw function
         function draw()
         {
+            //clear the canvas
+            jc.clear('canvas_1');
             //update the postion of the turtle
             updateTurtle();
             jc.start('canvas_1');
             jc.rect(turtle['x'],turtle['y'],turtle['bound_width'],turtle['bound_height'],1).rotate(dr,'center');
             jc.rect(150,40,50,60,1).rotate(30,'center');
             jc.start('canvas_1');
-        }
+        };
 
+
+        //on a key press event record the key that was pressed into the
+        //"keys" object
         function checkKey(e) 
         {
                 e = e || window.event;
-                if(e.keyCode == 39) {
-                    keys['right'] = true;
-                    console.log('sdads');
-                };
-                if(e.keyCode == 37) {
-                    keys['left'] = true;
-                };
-                if(e.keyCode == 38) {
-                    keys['up'] = true;
-                };
-                if(e.keyCode == 40) {
-                    keys['down'] = true;
-                };
-                jc.clear('canvas_1');
-                
-                draw();
-        }
 
+                if(e.keyCode == 39) {   keys['right'] = true;   };
+                if(e.keyCode == 37) {   keys['left'] = true;    };
+                if(e.keyCode == 38) {   keys['up'] = true;      };
+                if(e.keyCode == 40) {   keys['down'] = true;    };
+
+                draw();
+        };
+
+        //indicate that a key is no longer being pressed by falsifying
+        //its value in the "keys" object
         function removeKey(e)
         {
                 e = e || window.event;
-                if(e.keyCode == 39) {
-                    keys['right'] = false;
-                };
-                if(e.keyCode == 37) {
-                    keys['left'] = false;
-                };
-                if(e.keyCode == 38) {
-                    keys['up'] = false;
-                };
-                if(e.keyCode == 40) {
-                    keys['down'] = false;
-                };
+
+                if(e.keyCode == 39) {   keys['right'] = false;  };
+                if(e.keyCode == 37) {   keys['left'] = false;   };
+                if(e.keyCode == 38) {   keys['up'] = false;     };
+                if(e.keyCode == 40) {   keys['down'] = false;   };
+
                 draw();
-        }
+        };
 
         function updateTurtle(){
         
-                if(keys['right'] == true) {
-                    dr+=rot_increment;
-                };
-                if(keys['left'] == true) {
-                    dr-=rot_increment;
-                };
+                //if the left or right keys clicked then rotate the turtle
+                if(keys['right'] == true)   {   dr+=rot_increment;  };
+                if(keys['left'] == true)    {   dr-=rot_increment;  };
+
+                //move forwards relative to the facing direction
                 if(keys['up'] == true) {
-                   
                     turtle['x'] += Math.sin(dr * Math.PI / 180)*5;
                     turtle['y'] -= Math.cos(dr * Math.PI / 180)*5;
                 };
+
+                //move backwards
                 if(keys['down'] == true) {
-                   
                     turtle['x'] -= Math.sin(dr * Math.PI / 180)*5;
                     turtle['y'] += Math.cos(dr * Math.PI / 180)*5;
                 };
         };
-
-
-            
